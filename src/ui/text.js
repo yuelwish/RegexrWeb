8384ee0cdff6ef02714d3dfe02936700626ab44b
yuelwish <zhenhun.yes@163.com>
2026-06-20 23:00:28 +0800

    fix: 删除重复的 SAMPLE_TEXT

--- src/ui/text.js
+++ src/ui/text.js
@@ -27,12 +27,6 @@ The side bar includes a Cheatsheet, full Reference, and Help. You can also Save 
 
 Explore results with the Tools below. Replace & List output custom results. Details lists capture groups. Explain describes your expression in plain English.`;
 
-Edit the Expression & Text to see matches. Roll over matches or the expression for details. PCRE & JavaScript flavors of RegEx are supported. Validate your expression with Tests mode.
-
-The side bar includes a Cheatsheet, full Reference, and Help. You can also Save & Share with the Community and view patterns you create or favorite in My Patterns.
-
-Explore results with the Tools below. Replace & List output custom results. Details lists capture groups. Explain describes your expression in plain English.`;
-
 export class TextUI {
   constructor(container) {
     this.container = container;
