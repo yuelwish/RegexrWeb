d03c3b6955f515e16ea349b2c89efbf612d47fff
yuelwish <zhenhun.yes@163.com>
2026-06-20 22:59:05 +0800

    fix: 恢复 SAMPLE_TEXT 定义

--- src/ui/text.js
+++ src/ui/text.js
@@ -19,6 +19,14 @@ const decorationField = StateField.define({
   },
 });
 
+const SAMPLE_TEXT = `RegExr was created by gskinner.com.
+
+Edit the Expression & Text to see matches. Roll over matches or the expression for details. PCRE & JavaScript flavors of RegEx are supported. Validate your expression with Tests mode.
+
+The side bar includes a Cheatsheet, full Reference, and Help. You can also Save & Share with the Community and view patterns you create or favorite in My Patterns.
+
+Explore results with the Tools below. Replace & List output custom results. Details lists capture groups. Explain describes your expression in plain English.`;
+
 Edit the Expression & Text to see matches. Roll over matches or the expression for details. PCRE & JavaScript flavors of RegEx are supported. Validate your expression with Tests mode.
 
 The side bar includes a Cheatsheet, full Reference, and Help. You can also Save & Share with the Community and view patterns you create or favorite in My Patterns.
